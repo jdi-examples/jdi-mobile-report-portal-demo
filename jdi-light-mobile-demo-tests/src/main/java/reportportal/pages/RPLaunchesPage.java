36f62d5e64f5bae0a196c950d31376761f96c416
alexarm <alex.armenshin@gmail.com>
2020-06-10 12:45:31 +0300

    update tests

--- jdi-light-mobile-demo-tests/src/main/java/reportportal/pages/RPLaunchesPage.java
+++ jdi-light-mobile-demo-tests/src/main/java/reportportal/pages/RPLaunchesPage.java
@@ -1,10 +1,15 @@
 package reportportal.pages;
 
+import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath;
 import com.epam.jdi.light.mobile.elements.common.Text;
 
+import java.util.List;
+
 public class RPLaunchesPage extends WebPage {
     @XPath("//*[@class='noItemMessage__message--2ydYP']")
     public Text noResultsFoundLabel;
+    @XPath("//div[contains(@class, 'itemInfo__main-info')]")
+    public List<Label> launchNames;
 }
