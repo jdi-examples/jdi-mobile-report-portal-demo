20c8b34bfb2fd5991ca1463c7f4d43947d6fd7a9
Natalia_Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-04-01 11:57:58 +0300

    Update libs to current versions

--- jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/driver/MobileDriverInfos.java
+++ jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/driver/MobileDriverInfos.java
@@ -8,10 +8,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
 
-import static com.epam.jdi.light.driver.get.DriverData.*;
+import static com.epam.jdi.light.driver.get.DriverData.getCapabilities;
 import static com.epam.jdi.light.driver.get.DriverTypes.CHROME;
-import static com.epam.jdi.light.driver.get.RemoteDriver.*;
-import static com.epam.jdi.light.mobile.driver.MobileDriverData.*;
+import static com.epam.jdi.light.driver.get.DriverTypes.SAFARI;
+import static com.epam.jdi.light.driver.get.RemoteDriverInfo.getRemoteURL;
+import static com.epam.jdi.light.mobile.driver.MobileDriverData.ANDROID_OPTIONS;
+import static com.epam.jdi.light.mobile.driver.MobileDriverData.IOS_OPTIONS;
 
 /**
  * Created by Roman Iovlev on 26.09.2019
@@ -22,17 +24,18 @@ public class MobileDriverInfos {
         .set(d -> {
             d.downloadType = CHROME;
             d.initCapabilities = new DesiredCapabilities();
-            d.path = DriverData::chromeDriverPath;
             d.capabilities = c -> getCapabilities(c, ANDROID_OPTIONS::execute);
-//            d.properties = "";
+            d.properties = "webdriver.chrome.driver";
+            d.path = DriverData::chromeDriverPath;
             d.getRemoteDriver = c -> new AndroidDriver<>(new URL(getRemoteURL()), c);
         }
     );
 public static DriverInfo IOS_INFO = new DriverInfo()
         .set(d -> {
-            d.downloadType = CHROME;
+            d.downloadType = SAFARI;
             d.initCapabilities = new DesiredCapabilities();
             d.capabilities = c -> getCapabilities(c, IOS_OPTIONS::execute);
+            d.properties = "webdriver.safari.driver";
             d.path = DriverData::safariDriverPath;
             d.getRemoteDriver = c -> new IOSDriver<>(new URL(getRemoteURL()), c);
         }
--- jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/composite/MobileScreen.java
+++ jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/composite/MobileScreen.java
@@ -1,7 +1,9 @@
 package com.epam.jdi.light.mobile.elements.composite;
 
+import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.mobile.MobileContextHolder;
+import com.epam.jdi.light.mobile.elements.common.app.Button;
 import io.appium.java_client.MultiTouchAction;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
@@ -12,18 +14,17 @@ import io.appium.java_client.screenrecording.BaseStopScreenRecordingOptions;
 import io.appium.java_client.screenrecording.CanRecordScreen;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
-import org.apache.commons.lang3.NotImplementedException;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.function.Function;
 
 import static com.epam.jdi.light.common.Exceptions.exception;
 import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 import static com.epam.jdi.light.mobile.MobileUtils.executeDriverMethod;
 import static java.lang.Math.round;
+import static java.time.Duration.ofMillis;
 
 public class MobileScreen {
 
@@ -45,9 +46,16 @@ public class MobileScreen {
                 throw exception("Cannot use this method. The driver needs to extend/implement the AppiumDriver class");
             }
             screenCenter = new Point(screenSize.width / 2, screenSize.height / 2);
+
         }
         return screenSize;
     }
+
+    private static int getHeight() {
+        Dimension dimension = getScreenSize();
+        return dimension.getHeight();
+    }
+
     @JDIAction
     private static void scroll(Point startingPoint, int xOffset, int yOffset) {
         executeDriverMethod(PerformsTouchActions.class, (PerformsTouchActions driver) -> {
@@ -58,6 +66,7 @@ public class MobileScreen {
                     .release().perform();
         });
     }
+
     private static void scrollVertically(int offset) {
         Dimension dimension = getScreenSize();
         int height = dimension.getHeight();
@@ -66,11 +75,12 @@ public class MobileScreen {
         }
         scroll(screenCenter, 0, offset % (height / 2));
     }
+
     private static void scrollHorizontally(int offset) {
         Dimension dimension = getScreenSize();
         int width = dimension.getWidth();
         for (int i = 0; i < Math.abs(offset) / (width / 2); i++) {
-            scroll(screenCenter, Integer.signum(offset) * ( width / 2 - 1), 0);
+            scroll(screenCenter, Integer.signum(offset) * (width / 2 - 1), 0);
         }
         scroll(screenCenter, offset % (width / 2), 0);
     }
@@ -79,24 +89,94 @@ public class MobileScreen {
     public static void scrollUp(int offset) {
         scrollVertically(offset);
     }
+
     @JDIAction("Scroll down by {0} px")
     public static void scrollDown(int offset) {
         scrollVertically(-offset);
     }
+
     @JDIAction("Scroll right by {0} px")
     public static void scrollRight(int offset) {
         scrollHorizontally(-offset);
     }
+
     @JDIAction("Scroll left by {0} px")
     public static void scrollLeft(int offset) {
         scrollHorizontally(offset);
     }
 
-    public static void scrollToTop() {
-        throw new NotImplementedException("Not implemented yet");
+    //Android
+    public static void swipeToElement(WebElement startElement, WebElement endElement) {
+        int startX = startElement.getLocation().getX() + (startElement.getSize().getWidth() / 2);
+        int startY = startElement.getLocation().getY() + (startElement.getSize().getHeight() / 2);
+
+        int endX = endElement.getLocation().getX() + (endElement.getSize().getWidth() / 2);
+        int endY = endElement.getLocation().getY() + (endElement.getSize().getHeight() / 2);
+
+        executeDriverMethod(PerformsTouchActions.class, (PerformsTouchActions driver) -> {
+            new TouchAction<>(driver)
+                    .press(PointOption.point(startX, startY))
+                    .waitAction(WaitOptions.waitOptions(ofMillis(300)))
+                    .moveTo(PointOption.point(endX, endY))
+                    .release().perform();
+        });
     }
+
+    //Works only if sought element exists.
+    public static void scrollDownToXpath(String xpath) {
+        int scrollDivider = 3;
+        int height = getHeight();
+        while (true) {
+            List<WebElement> soughtElement = getDriver().findElements(By.xpath(xpath));
+            if (!soughtElement.isEmpty()) {
+                return;
+            }
+            scrollDown(height / scrollDivider);
+        }
+    }
+
+    public static UIAssert scrollToElementInList(Button element) {
+        int shift = getHeight()/2-1;
+        String oldPage = getDriver().getPageSource();
+        while (true) {
+            try {
+                element.isEnabled();
+                return new UIAssert().set(element);
+            } catch (RuntimeException e) {
+                MobileScreen.scrollDown(shift);
+                String newPage = getDriver().getPageSource();
+                if (oldPage.equals(newPage)) {
+                    return new UIAssert().set(element);
+                }
+                oldPage = newPage;
+            }
+        }
+    }
+
+    public static void scrollToTop() {
+        int shift = 1000;
+        String oldPage = getDriver().getPageSource();
+        while (true) {
+            MobileScreen.scrollDown(-shift);
+            String newPage = getDriver().getPageSource();
+            if (oldPage.equals(newPage)) {
+                return;
+            }
+            oldPage = newPage;
+        }
+    }
+
     public static void scrollToBottom() {
-        throw new NotImplementedException("Not implemented yet");
+        int shift = 1000;
+        String oldPage = getDriver().getPageSource();
+        while (true) {
+            MobileScreen.scrollDown(shift);
+            String newPage = getDriver().getPageSource();
+            if (oldPage.equals(newPage)) {
+                return;
+            }
+            oldPage = newPage;
+        }
     }
 
     private static void pinch(Point startPoint1, Point startPoint2, Point endPoint1, Point endPoint2) {
@@ -114,21 +194,24 @@ public class MobileScreen {
             new MultiTouchAction(driver).add(ta1).add(ta2).perform();
         });
     }
+
     @JDIAction("Zoom in")
     public static void zoomIn() {
         Dimension dimension = getScreenSize();
         pinch(screenCenter.moveBy(1, 1), screenCenter,
                 new Point(dimension.width - 1, dimension.height - 1), new Point(0, 0));
     }
+
     @JDIAction("Zoom out")
     public static void zoomOut() {
         Dimension dimension = getScreenSize();
         pinch(new Point(dimension.width - 1, dimension.height - 1), new Point(0, 0),
                 screenCenter, screenCenter);
     }
+
     @JDIAction("Zoom in by {0}")
     public static void zoomIn(double ratio) {
-        if ((ratio < 0 ) || (ratio > 1)) {
+        if ((ratio < 0) || (ratio > 1)) {
             throw exception("The zoom ratio needs to be between 0 and 1");
         } else {
             Dimension dimension = getScreenSize();
@@ -137,9 +220,10 @@ public class MobileScreen {
                     screenCenter.moveBy(-(int) round(ratio * dimension.width / 2), -(int) round(ratio * dimension.height / 2)));
         }
     }
+
     @JDIAction("Zoom out by {0}")
     public static void zoomOut(double ratio) {
-        if ((ratio < 0 ) || (ratio > 1)) {
+        if ((ratio < 0) || (ratio > 1)) {
             throw exception("The zoom ratio needs to be between 0 and 1");
         } else {
             Dimension dimension = getScreenSize();
@@ -153,16 +237,19 @@ public class MobileScreen {
         return executeDriverMethod(CanRecordScreen.class,
                 (Function<CanRecordScreen, String>) CanRecordScreen::startRecordingScreen);
     }
+
     public static <T extends BaseStartScreenRecordingOptions<?>> String startRecordingScreen(T options) {
         return executeDriverMethod(CanRecordScreen.class,
-                (CanRecordScreen driver) ->  driver.startRecordingScreen(options));
+                (CanRecordScreen driver) -> driver.startRecordingScreen(options));
     }
+
     public static String stopRecordingScreen() {
         return executeDriverMethod(CanRecordScreen.class,
                 (Function<CanRecordScreen, String>) CanRecordScreen::stopRecordingScreen);
     }
+
     public static <T extends BaseStopScreenRecordingOptions<?>> String stopRecordingScreen(T options) {
         return executeDriverMethod(CanRecordScreen.class,
-                (CanRecordScreen driver) ->  driver.stopRecordingScreen(options));
+                (CanRecordScreen driver) -> driver.stopRecordingScreen(options));
     }
 }
